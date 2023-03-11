974b332ba7b7e3f04f0f9e0058381bae1fbf81b5
phildan02 <phildan02@gmail.com>
2023-03-11 14:45:13 +0300

    first commit

---
+++ citilink.py
@@ -0,0 +1,73 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium_stealth import stealth
+import time
+
+
+options = webdriver.ChromeOptions()
+options.add_argument("start-maximized")
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+options.add_argument("--headless")
+
+driver = webdriver.Chrome(options=options)
+
+stealth(driver,
+        languages=["en-US", "en"],
+        vendor="Google Inc.",
+        platform="Win32",
+        webgl_vendor="Intel Inc.",
+        renderer="Intel Iris OpenGL Engine",
+        fix_hairline=True
+        )
+
+
+driver.get("https://www.citilink.ru/catalog/kompyutery/")
+
+try:
+    targetElem = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, 'e1j9birj0')))
+
+
+    totalNumElems = int(driver.find_element(By.CLASS_NAME, 'e1di3r8d0').text[:-8])
+    numElems = 48
+
+    if totalNumElems < numElems:
+        numElems = totalNumElems
+
+    elems = driver.find_elements(By.CSS_SELECTOR, '.e1j9birj0.e106ikdt0.app-catalog-175fskm.e1gjr6xo0')
+
+    while len(elems) < numElems:
+        time.sleep(0.5)
+        elems = driver.find_elements(By.CSS_SELECTOR, '.e1j9birj0.e106ikdt0.app-catalog-175fskm.e1gjr6xo0')
+        driver.execute_script("window.scrollBy(0, 400)")
+
+    while len(elems) > numElems:
+        elems.pop()
+
+    prices = []
+    for x in elems:
+        prices.append(x.text)
+
+    print(len(prices))
+    print(prices)
+    
+    # i = 0
+    # while i < len(prices):
+    #     index = prices[i].find("\n")
+    #     if index != -1:
+    #         prices[i] = prices[i][:(index - 2)]
+    #     else: i+=1
+
+    f = open('data-citilink.txt', 'w')
+    f.write(str(prices))
+    f.close()
+
+except TimeoutException:
+    print("Loading took too much time!")
+
+
+driver.quit()
---
+++ dns.py
@@ -0,0 +1,81 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium_stealth import stealth
+import time
+from tkinter import *
+from tkinter import ttk
+
+options = webdriver.ChromeOptions()
+options.add_argument("start-maximized")
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+options.add_argument("--headless")
+
+driver = webdriver.Chrome(options=options)
+
+stealth(driver,
+        languages=["en-US", "en"],
+        vendor="Google Inc.",
+        platform="Win32",
+        webgl_vendor="Intel Inc.",
+        renderer="Intel Iris OpenGL Engine",
+        fix_hairline=True
+        )
+
+
+def getPrices():
+    driver.get("https://www.dns-shop.ru/catalog/17a8932c16404e77/personalnye-kompyutery/")
+    try:
+        targetElem = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'product-buy__price')))
+
+        totalNumElems = int(driver.find_element(By.CLASS_NAME, 'products-count').text[:-8])
+        numElems = 18
+
+        if totalNumElems < numElems:
+            numElems = totalNumElems
+
+        elems = driver.find_elements(By.CLASS_NAME, 'product-buy__price')
+
+        while len(elems) != numElems:
+            elems = driver.find_elements(By.CLASS_NAME, 'product-buy__price')
+            time.sleep(0.5)
+            driver.execute_script("window.scrollBy(0, 800)")
+
+
+        prices = []
+        for x in elems:
+            prices.append(x.text[:-2])
+        
+        i = 0
+        while i < len(prices):
+            index = prices[i].find("\n")
+            if index != -1:
+                prices[i] = prices[i][:(index - 2)]
+            else: i+=1
+
+        # f = open('data-dns.txt', 'w')
+        # f.write(str(prices))
+        # f.close()
+
+        prcslabel["text"] = str(prices)
+
+    except TimeoutException:
+        prcslabel["text"]="Ошибка загрузки!"
+
+    driver.quit()
+
+
+root = Tk()
+root.title("Анализ цен")
+root.geometry("300x250")
+
+btn = ttk.Button(text="Получить цены", command=getPrices)
+btn.pack()
+prcslabel = ttk.Label()
+prcslabel.pack()
+
+root.mainloop()
