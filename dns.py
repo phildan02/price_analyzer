1b6c1cc12d219b5d2ead533fb2da8823cf65d448
phildan02 <phildan02@gmail.com>
2023-04-03 15:20:31 +0300

    checkbuttons

--- dns.py
+++ dns.py
@@ -27,17 +27,19 @@ stealth(driver,
 
 
 def getPrices():
-    driver.get("https://www.dns-shop.ru/catalog/17a8a01d16404e77/smartfony/?review=1")
+    driver.get(
+        "https://www.dns-shop.ru/catalog/17a8a01d16404e77/smartfony/?review=1")
     try:
         targetElem = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.CLASS_NAME, 'product-buy__price')))
 
-        strTotalNumElems = driver.find_element(By.CLASS_NAME, 'products-count').text
+        strTotalNumElems = driver.find_element(
+            By.CLASS_NAME, 'products-count').text
         if strTotalNumElems[-1] == "в":
             totalNumElems = int(strTotalNumElems[:-8])
         else:
             totalNumElems = int(strTotalNumElems[:-7])
-        
+
         numElems = 18
 
         if totalNumElems < numElems:
@@ -50,47 +52,109 @@ def getPrices():
             elems = driver.find_elements(By.CLASS_NAME, 'product-buy__price')
             driver.execute_script("window.scrollBy(0, 800)")
 
-
         prices = []
         for x in elems:
             prices.append(x.text[:-2])
-        
+
         i = 0
         while i < len(prices):
             index = prices[i].find("\n")
             if index != -1:
                 prices[i] = prices[i][:(index - 2)]
-            else: i+=1
+            else:
+                i += 1
 
         prcsLabel["text"] = f'{str(prices[0])} - {str(prices[-1])}'
 
     except TimeoutException:
-        prcsLabel["text"]="Ошибка загрузки!"
+        prcsLabel["text"] = "Ошибка загрузки!"
 
     driver.quit()
 
 
-
-def execution():
-    prcsLabel["text"]="Идёт поиск цен..."
-    getPrices()
-
-
 root = Tk()
 root.title("Анализ цен")
-root.geometry("300x250")
+root.geometry("500x200")
 
-prcsLabel = ttk.Label()
-prcsLabel.pack(anchor=NW)
+ctgsExplLabel = ttk.Label(text="Категория:")
+ctgsExplLabel.place(x=10, y=10)
 
-explLabel = ttk.Label(text="Выберите категорию товаров:")
-explLabel.pack(anchor=NW)
+categories = ["Мониторы", "Системные блоки", "Жёсткие диски"]
+ctgsBox = ttk.Combobox(values=categories, state="readonly")
+ctgsBox.place(x=80, y=10)
 
-categories = ["Системные блоки", "Мониторы", "Клавиатуры"]
-ctgsBox = ttk.Combobox(values=categories)
-ctgsBox.pack(anchor=NW)
+resourceFrame = ttk.LabelFrame(text="Ресурс", padding=[8, 4])
+resourceFrame.place(x=10, y=40)
 
-getBtn = ttk.Button(text="Получить цены", command=execution)
-getBtn.pack(anchor=NW)
+rsrcVars = []
+for t in range(3):
+    rsrcVars.append(BooleanVar())
+
+dnsCheckbtn = ttk.Checkbutton(resourceFrame, text="DNS", variable=rsrcVars[0])
+dnsCheckbtn.pack(anchor=W)
+
+citilinkCheckbtn = ttk.Checkbutton(resourceFrame, text="Ситилинк", variable=rsrcVars[1])
+citilinkCheckbtn.pack(anchor=W)
+
+mvideoCheckbtn = ttk.Checkbutton(resourceFrame, text="М.видео", variable=rsrcVars[2])
+mvideoCheckbtn.pack(anchor=W)
+
+
+prcMinExplLabel = ttk.Label(text="Мин.цена")
+prcMinExplLabel.place(x=120, y=65)
+prcMaxExplLabel = ttk.Label(text="Макс.цена")
+prcMaxExplLabel.place(x=120, y=95)
+
+prcMinEntry = ttk.Entry(width=10)
+prcMinEntry.place(x=190, y=65)
+prcMaxEntry = ttk.Entry(width=10)
+prcMaxEntry.place(x=190, y=95)
+
+
+brandFrame = ttk.LabelFrame(text="Бренд", padding=[8, 4])
+brandFrame.place(x=270, y=40)
+
+mntrVars = []
+for i in range(5):
+    mntrVars.append(BooleanVar())
+
+mntrAcerCheckbtn = ttk.Checkbutton(brandFrame, text="Acer", variable=mntrVars[0])
+mntrAcerCheckbtn.grid(row=0, column=0, sticky=W)
+
+mntrAocCheckbtn = ttk.Checkbutton(brandFrame, text="Aoc", variable=mntrVars[1])
+mntrAocCheckbtn.grid(row=1, column=0, sticky=W)
+
+mntrSamsungCheckbtn = ttk.Checkbutton(brandFrame, text="Samsung", variable=mntrVars[2])
+mntrSamsungCheckbtn.grid(row=2, column=0, sticky=W)
+
+mntrAsusCheckbtn = ttk.Checkbutton(brandFrame, text="Asus", variable=mntrVars[3])
+mntrAsusCheckbtn.grid(row=0, column=1, sticky=W)
+
+mntrDellCheckbtn = ttk.Checkbutton(brandFrame, text="Dell", variable=mntrVars[4])
+mntrDellCheckbtn.grid(row=1, column=1, sticky=W)
+
+
+mntrCheckbtns = [mntrAcerCheckbtn, mntrAocCheckbtn, mntrSamsungCheckbtn, mntrAsusCheckbtn, mntrDellCheckbtn]
+
+
+def mntrAllFunc():
+    j = 0
+    if mntrAllVar.get() == 1:
+        for j in range(len(mntrCheckbtns)):
+            mntrVars[j].set(1)
+            mntrCheckbtns[j]["state"] = DISABLED
+    else:
+        for j in range(len(mntrCheckbtns)):
+            mntrCheckbtns[j]["state"] = NORMAL
+
+
+mntrAllVar = BooleanVar()
+mntrAllCheckbtn = ttk.Checkbutton(brandFrame, text="Все", variable=mntrAllVar, command=mntrAllFunc)
+mntrAllCheckbtn.grid(row=2, column=1, sticky=W)
+
+
+# getBtn = ttk.Button(text="Получить цены", command=getPrices)
+# getBtn.place(x=300, y=10)
+
 
 root.mainloop()
